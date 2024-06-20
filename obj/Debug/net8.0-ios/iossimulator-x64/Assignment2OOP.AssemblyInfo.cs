bf18103f89b9fd3a614c5c771e1d08239b5c2a3a
Carson Turner <73399912+CDanielTurner@users.noreply.github.com>
2024-06-19 19:34:42 -0600

    Flights UI Updated
    Flights Page added all components for UI. Added icons to each page, and changed title of project.

--- obj/Debug/net8.0-ios/iossimulator-x64/Assignment2OOP.AssemblyInfo.cs
+++ obj/Debug/net8.0-ios/iossimulator-x64/Assignment2OOP.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Assignment2OOP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d3dfe280ebc91e5e007a01b1bde83491b4d228de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+d68268b266115613204105f8de3b027f9e4f4f82")]
 [assembly: System.Reflection.AssemblyProductAttribute("Assignment2OOP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Assignment2OOP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
