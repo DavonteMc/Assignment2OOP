349e754ab795fbace3745b297623e7024acbd500
OOP Project <davontemc.suf@gmail.com>
2024-06-24 09:02:31 -0600

    Added Airport, AirportManager, Flight and FlightManager Classes - attempt 2

---
+++ Data/Airport.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2OOP.Data
+{
+    public class Airport
+    {
+        string code;
+        string name;
+
+        public string Code { get; set; }
+        public string Name { get; set; }
+
+        public Airport() { }
+        public Airport(string code, string name)
+        {
+            this.Code = code;
+            this.Name = name;
+        }
+    }
+}
---
+++ Data/AirportManager.cs
@@ -0,0 +1,33 @@
+﻿using Assignment2OOP.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment2OOP.Data
+{
+    public class AirportManager
+    {
+        string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..\\..\\..\\..\\..\\Resources\\Res\\airports.csv");
+        public static List<Airport> airports = new List<Airport>();
+
+        public AirportManager()
+        {
+            PopulateAirports();
+        }
+
+        public void PopulateAirports()
+        {
+            Airport airport;
+            foreach (string line in File.ReadAllLines(filePath))
+            {
+                string[] parts = line.Split(",");
+                airport = new Airport(parts[0], parts[1]);
+                airports.Add(airport);
+            }
+        }
+
+        public static List<Airport> GetAirports() { return airports; }
+    }
+}
